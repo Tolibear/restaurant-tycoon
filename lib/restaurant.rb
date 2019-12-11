169fa9404bdc7d08d6f9f7b415b4ce5bbc557cd1
flatironschool <t.benson@live.co.uk>
2019-12-11 22:56:13 +0000

    First attempt at writing Restaurant class with .all method. Not complete

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,20 @@
 
 class Restaurant
+
+    attr_accessor :name, :location
+    
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def locations
+
+    end
 end
